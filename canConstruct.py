61da975439572b90327464e8c1c7246e68e51f41
Khrystyna <khrystyna.koshova@gmail.com>
2026-01-20 22:31:02 +0100

    canConstruct memoization

--- canConstruct.py
+++ canConstruct.py
@@ -1,11 +1,15 @@
-def can_construct (target, word_bank):
+def can_construct (target, word_bank=[], memo={}):
+    if target in memo:
+        return memo[target]
     if target == '':
         return True
     for word in word_bank:
         if word_bank.index(word) == 0:
             suffix = target[len(word)::]
-            if can_construct(suffix, word_bank) == True:
+            if can_construct(suffix, word_bank, memo) == True:
+                memo[target] = True
                 return True
+    memo[target] = False
     return False
     
 if __name__ == "__main__":
