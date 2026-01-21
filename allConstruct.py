4ec225359ac8f9317a38290fd2dcfc3f86dc4ccf
Khrystyna <khrystyna.koshova@gmail.com>
2026-01-21 20:57:37 +0100

    allConstruct memoization

--- allConstruct.py
+++ allConstruct.py
@@ -1,13 +1,16 @@
-def all_construct(target, word_bank):
+def all_construct(target, word_bank=[], memo={}):
+    if target in memo:
+        return memo[target]
     if target == '':
         return [[]]
     result = []
     for word in word_bank:
         if target.startswith(word):
             suffix = target[len(word):]
-            suffix_ways = all_construct(suffix, word_bank)
+            suffix_ways = all_construct(suffix, word_bank, memo)
             target_ways = [[word, *way] for way in suffix_ways]
             result.extend(target_ways)
+    memo[target] = result
     return result
 
 if __name__ == "__main__":
