32c81e084081d37b6f8f8d22e9395e7256354d4d
Khrystyna <khrystyna.koshova@gmail.com>
2026-01-21 20:20:19 +0100

    countConstruct memoization

--- countConstruct.py
+++ countConstruct.py
@@ -1,12 +1,15 @@
-def count_construct(target, word_bank=[]):
+def count_construct(target, word_bank=[], memo={}):
+    if target in memo:
+        return memo[target]
     if target == '':
         return 1
     total_count = 0
     for word in word_bank:
         if target.startswith(word):
             suffix = target[len(word):]
-            num_ways_for_rest = count_construct(suffix, word_bank)
+            num_ways_for_rest = count_construct(suffix, word_bank, memo)
             total_count += num_ways_for_rest
+    memo[target] = total_count
     return total_count
 
 if __name__ == "__main__":
