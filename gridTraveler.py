278c558dd7967399bce8231785c1860706dd65a4
Khrystyna <khrystyna.koshova@gmail.com>
2026-01-22 21:18:48 +0100

    gridTraveler tabulation

--- gridTraveler.py
+++ gridTraveler.py
@@ -1,3 +1,4 @@
+# Memoization
 def gridTravel (m, n, memo = {}):
     key = str(m) + ',' + str(n)
 
@@ -13,5 +14,19 @@ def gridTravel (m, n, memo = {}):
     memo[key] = gridTravel(m - 1, n, memo) + gridTravel(m, n - 1, memo)
     return memo[key]
 
+# Tabulation
+def grid_traveler_tabulation(m, n):
+    table = [[0] * (n + 1) for _ in range(m + 1)]
+    table[1][1] = 1
+    for i in range(m+1):
+        for j in range(n+1):
+            current = table[i][j]
+            if (j+1 <= n):
+                table[i][j+1] += current
+            if (i+1 <= m):
+                table[i+1][j] += current
+    return table[m][n]
+
 if __name__ == "__main__":
-    print(gridTravel(18, 18))
+    #print(gridTravel(18, 18))
+    print(grid_traveler_tabulation(3, 3))
