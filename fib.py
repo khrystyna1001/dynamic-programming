6e40c37f01eebb34f78170e0a871eff98a29dc0d
Khrystyna <khrystyna.koshova@gmail.com>
2026-01-22 20:43:43 +0100

    tabulation for fib function

--- fib.py
+++ fib.py
@@ -1,3 +1,4 @@
+# Memoization
 def fib(n, memo = {}):
     if (n in memo):
         return memo[n]
@@ -7,5 +8,16 @@ def fib(n, memo = {}):
     memo[n] = fib(n-1, memo) + fib(n-2, memo)
     return memo[n]
 
+# Tabulation
+def fib_tabulation(n):
+    table = [0] * (n+3)
+    table[1] = 1
+    for i in range(n):
+        table[i+1] += table[i]
+        table[i+2] += table[i]
+
+    return table[n]
+
 if __name__ == "__main__":
-    print(fib(50))
+    # print(fib(50))
+    print(fib_tabulation(6))
