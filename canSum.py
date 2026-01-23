a7847450f5d4eae56729384d1c9a13261ea9177a
Khrystyna <khrystyna.koshova@gmail.com>
2026-01-23 19:30:11 +0100

    canSum tabulation

--- canSum.py
+++ canSum.py
@@ -1,3 +1,4 @@
+# Memoization
 def canSum(targetSum, numbers=[], memo={}):
     if targetSum in memo:
         return memo[targetSum]
@@ -14,6 +15,17 @@ def canSum(targetSum, numbers=[], memo={}):
     memo[targetSum] = False
     return False
 
+# Tabulation
+def can_sum_tabulation(target, numbers):
+    table = [False]*(target+1)
+    table[0] = True
+    for i in range(len(table)):
+        if (table[i] == True):
+            for num in numbers:
+                if i+num <= target:
+                    table[i+num] = True
+    return table[target]
 
 if __name__ == "__main__":
-    print(canSum(7, [2,3,4,5]))
+    # print(canSum(7, [2,3,4,5]))
+    print(can_sum_tabulation(7, [2,3,4,5]))
