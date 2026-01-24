86fe2db6d6bb5b01aa932a2399c3e525f4c64f52
Khrystyna <khrystyna.koshova@gmail.com>
2026-01-24 21:24:29 +0100

    bestSum tabulation

--- bestSum.py
+++ bestSum.py
@@ -1,3 +1,4 @@
+# Memoization
 def bestSum(targetSum, numbers=[], memo={}):
     if targetSum in memo:
         return memo[targetSum]
@@ -20,6 +21,22 @@ def bestSum(targetSum, numbers=[], memo={}):
     memo[targetSum] = shortest
     return shortest
 
+# Tabulation
+def best_sum_tabulation(target, numbers=[]):
+    table = [None] * (target + 1)
+    table[0] = []
+
+    for i in range(target):
+        if table[i] is not None:
+            for num in numbers:
+                if i + num <= target:
+                    combination = [*table[i], num]
+                    if table[i+num] is None or len(table[i + num]) > len(combination):
+                        table[i+num] = combination
+    return table[target]
+
 if __name__ == "__main__":
-    print(bestSum(7, [5,3,4,7]))
-    print(bestSum(8, [2,3,5]))
+    # print(bestSum(7, [5,3,4,7]))
+    # print(bestSum(8, [2,3,5]))
+    print(best_sum_tabulation(7, [5,3,4,7]))
+    print(best_sum_tabulation(8, [2,3,5]))
