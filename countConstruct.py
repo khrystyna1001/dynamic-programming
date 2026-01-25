b2574358aad4eb1b6d15cee86e787b29127c678f
Khrystyna <khrystyna.koshova@gmail.com>
2026-01-25 22:10:01 +0100

    countConstruct tabulation

--- countConstruct.py
+++ countConstruct.py
@@ -1,3 +1,4 @@
+# Memoization
 def count_construct(target, word_bank=[], memo={}):
     if target in memo:
         return memo[target]
@@ -12,5 +13,16 @@ def count_construct(target, word_bank=[], memo={}):
     memo[target] = total_count
     return total_count
 
+# Tabulation
+def count_construct_tabulation(target, word_bank=[]):
+    table = [0] * (len(target) + 1)
+    table[0] = 1
+    for i in range(len(target)):
+        for word in word_bank:
+            if target[i:i + len(word)] == word:
+                table[i + len(word)] += table[i]
+    return table[len(target)]
+
 if __name__ == "__main__":
-    print(count_construct('purple', ['purp', 'p', 'ur', 'le', 'purpl']))
+    # print(count_construct('purple', ['purp', 'p', 'ur', 'le', 'purpl']))
+    print(count_construct_tabulation('purple', ['purp', 'p', 'ur', 'le', 'purpl']))
