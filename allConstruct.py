3c2f2616904deff8bec3b5c7cf0dfdf5eca50ba6
Khrystyna <khrystyna.koshova@gmail.com>
2026-01-26 20:49:02 +0100

    allConstruct tabulation

--- allConstruct.py
+++ allConstruct.py
@@ -1,3 +1,4 @@
+# Memoization
 def all_construct(target, word_bank=[], memo={}):
     if target in memo:
         return memo[target]
@@ -13,5 +14,18 @@ def all_construct(target, word_bank=[], memo={}):
     memo[target] = result
     return result
 
+# Tabulation
+def all_construct_tabulation(target, word_bank=[]):
+    table = [[] for _ in range(len(target) + 1)]
+    table[0] = [[]]
+    for i in range(len(target)):
+        for word in word_bank:
+            if target[i:i + len(word)] == word:
+                new_combinations = [sub_array + [word] for sub_array in table[i]]
+                if i + len(word) <= len(target):
+                    table[i + len(word)].extend(new_combinations)
+    return table[len(target)]
+
 if __name__ == "__main__":
-    print(all_construct('purple', ['purp', 'p', 'ur', 'le', 'purpl']))
+    # print(all_construct('purple', ['purp', 'p', 'ur', 'le', 'purpl']))
+    print(all_construct_tabulation('purple', ['purp', 'p', 'ur', 'le', 'purpl']))
